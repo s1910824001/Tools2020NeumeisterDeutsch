eefbbb364c8d615ef9f37a122ab6be0d4cd73b13
S1910824010 <S1910824010@students.fh-hagenberg.at>
2020-05-04 15:52:55 +0200

    bugfix

--- S1910824010_s1910824001_android/dummyAndroidProject.cpp
+++ S1910824010_s1910824001_android/dummyAndroidProject.cpp
@@ -2,6 +2,7 @@
 void thisIsANewMethod() {
 <<<<<<< HEAD
   std::cout << "This is the line of Michael!\n";
+  int bugFix = 5;
 =======
 	std::cout << "This is the line of Pascal" << std::endl;
 >>>>>>> af051c671d767628e389b8d46ae402114d2b81b3
