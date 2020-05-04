9f765243873753117089de183b9cd49dddc46b91
S1910824010 <S1910824010@students.fh-hagenberg.at>
2020-05-04 15:55:01 +0200

    bugfix

--- S1910824010_s1910824001_android/dummyAndroidProject.cpp
+++ S1910824010_s1910824001_android/dummyAndroidProject.cpp
@@ -3,6 +3,7 @@ void thisIsANewMethod() {
 <<<<<<< HEAD
   std::cout << "This is the line of Michael!\n";
   int bugFix = 5;
+  std::cout << "4 * bugfix = " << bugfix * 4 << std::endl; 
 =======
 	std::cout << "This is the line of Pascal" << std::endl;
 >>>>>>> af051c671d767628e389b8d46ae402114d2b81b3
